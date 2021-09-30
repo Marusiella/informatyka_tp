00d784123fee175628c7a7517cd9d6893c3fbac0
mark <mareczektoja@protonmail.com>
2021-09-30 17:13:11 +0200

    init

---
+++ obnizka_rust/target/debug/deps/obnizka_rust-17aa971ac05053d7.d
@@ -0,0 +1,5 @@
+/home/mark/Dokumenty/GitHub/Marusiella/informatyka_tp/obnizka_rust/target/debug/deps/obnizka_rust-17aa971ac05053d7.rmeta: src/main.rs
+
+/home/mark/Dokumenty/GitHub/Marusiella/informatyka_tp/obnizka_rust/target/debug/deps/obnizka_rust-17aa971ac05053d7.d: src/main.rs
+
+src/main.rs:
---
+++ obnizka_rust/target/debug/deps/obnizka_rust-8bde91807c26b780.d
@@ -0,0 +1,5 @@
+/home/mark/Dokumenty/GitHub/Marusiella/informatyka_tp/obnizka_rust/target/debug/deps/obnizka_rust-8bde91807c26b780.rmeta: src/main.rs
+
+/home/mark/Dokumenty/GitHub/Marusiella/informatyka_tp/obnizka_rust/target/debug/deps/obnizka_rust-8bde91807c26b780.d: src/main.rs
+
+src/main.rs:
